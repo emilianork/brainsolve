96d5f805d7e2e841b09dc01799b4226e26deed0d
Emiliano Cabrera <emilianork@togetic.(none)>
2013-10-02 13:36:46 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brainsolve::Application.config.secret_key_base = 'c46d05118a2fb7f0d44b810d66aef1193495c0a409039921ef2ec64ac83354926c5eddd0cc8016776a1302b0f74c55c1f3a2a87baca8924652455a72a388046c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brainsolve::Application.config.session_store :cookie_store, key: '_brainsolve_session'
