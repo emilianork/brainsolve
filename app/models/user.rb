2f97e75d83903601849ff5367ca0671b0f2145b5
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2013-10-10 13:30:43 -0500

    Current User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # Quitamos :recoverable, :trackable, 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :validatable
 
   has_many :problems
   has_many :solutions
