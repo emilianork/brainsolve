16f209683d8e309c9daccf0cd7d8d03469608954
Emiliano <jemiliano.cabrera@gmail.com>
2013-10-13 16:51:17 -0500

    Resuelto merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131013185602) do
+ActiveRecord::Schema.define(version: 20131013213531) do
 
   create_table "problems", force: true do |t|
     t.text     "title",              null: false
