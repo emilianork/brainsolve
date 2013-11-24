5077397b741f0d2c613e62b79bc638319d06f9c5
lucakronos <bere_rmts@hotmail.com>
2013-11-24 13:37:38 -0600

    Agregando mensajes de notificación

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117032545) do
+ActiveRecord::Schema.define(version: 20131117091434) do
 
   create_table "areas_of_knowledges", force: true do |t|
     t.string   "name"
