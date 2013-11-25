7aa6d74afbfef5475ae36182db24851848cc5a62
Emiliano <jemiliano.cabrera@gmail.com>
2013-11-24 22:42:57 -0600

    Flash separados

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
 
 	def create
+    flash[:error] = "ACA PASO ACA PASO2"
+    flash[:notice] = "ACA PASO1"
 		puts "aca paso"
 		build_resource(sign_up_params)
     	if resource.save
