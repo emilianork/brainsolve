e42b75ce537a224784d2e059700ad78dee236531
Emiliano <jemiliano.cabrera@gmail.com>
2013-11-24 23:28:49 -0600

    Tareas de erika terminadas

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
 
 	def create
-    flash[:error] = ["ACA PASO", "ACA PASO2"]
-    flash[:notice] = "ACA PASO1"
-		puts "aca paso"
+
 		build_resource(sign_up_params)
+
     	if resource.save
 
       		yield resource if block_given?
@@ -19,8 +18,21 @@ class RegistrationsController < Devise::RegistrationsController
         		respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       		end
     	else
-    		puts "ACA PASO 1"
+
       		clean_up_passwords resource
+          flash[:error] = []
+          resource.errors.messages.each do |key, messages|
+            messages.each do |message|
+            flash[:error] << "Email: #{message}." if key.to_s == "email"
+            flash[:error] << "Apodo: #{message}." if key.to_s == "nickname"
+            flash[:error] << "Contraseña: #{message}." if key.to_s == "password"
+            flash[:error] << "Verifica contraseña: #{message[0..10] + "n las contraseñas"}." if key.to_s == "password_confirmation"
+            flash[:error] << "Nombre(s): #{message}." if key.to_s == "first_name"
+            flash[:error] << "Apellido(s) paterno: #{message}." if key.to_s == "second_name"
+            flash[:error] << "Apellido(s) materno: #{message}." if key.to_s == "third_name"
+            flash[:error] << "Telefono: #{message}." if key.to_s == "telephone"
+            end
+          end
       		respond_with resource
     	end
 	end
