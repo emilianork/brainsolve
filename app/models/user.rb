8a686c3101b32c347243f7196be234aecb2a95ea
Emiliano <jemiliano.cabrera@gmail.com>
2013-11-24 20:27:51 -0600

    Corregido el registro de usuario, ya funciona

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ActiveRecord::Base
   attr_accessible :telephone, :id, :email, :password, :password_confirmation, :encrypted_password, :nickname, :first_name, :second_name, :third_name, :role
 
   validates :nickname, format: {with: /(\A((([a-zA-Z]|\d)+)\s*)\z)|\A\z/,message: "Formato inválido"}, presence: true	
-  validates :telephone, format: {with: /(\A(((\d{8})|((\d{2}-){3}\d{2})){1}\s*)\z)|(\A\z)/, message: "Formato inválido"}, presence: true
+  validates :telephone, format: {with: /(\A(((\d{8})|((\d{2}-){3}\d{2})){1}\s*)\z)|(\A\z)/, message: "Formato inválido"}
   validates :first_name, format: {with: /\A[\D]*\z/, message: "Formato inválido"} 
   validates :second_name, format: {with: /\A[\D]*\z/, message: "Formato inválido"}
   validates :third_name, format: {with: /\A[\D]*\z/, message: "Formato inválido"}
+  
   has_many :problems, :dependent => :destroy
   has_many :solutions, :dependent => :destroy
   has_many :notifications, :dependent => :destroy
